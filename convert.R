38b7e1e7cb3da67625fa4425ac9d31f296a5daea
Albert Sun <albert.sun310@duke.edu>
2020-12-08 16:10:46 -0800

    convert new data file

--- convert.R
+++ convert.R
@@ -1,4 +1,9 @@
 # convert from sas7bdat to csv
+
 library(haven)
-data <- read_sas("hxfel9919_request.sas7bdat")
-write.csv(data,"data.csv", row.names = FALSE)
+
+#data <- read_sas("hxfel9919_request.sas7bdat")
+#write.csv(data,"data.csv", row.names = FALSE)
+
+misdemeanor_data <- read_sas("hxmisd9919_request.sas7bdat")
+write.csv(misdemeanor_data,"misdemeanor_data.csv", row.names = FALSE)
